0cbfbfc1422f175763366e5bed6cba815c8652bf
Andrew Kordik <hepaxcodex@gmail.com>
2011-08-16 23:30:11 -0400

    Forgot something

--- MATLAB/HistogramCalculation.m
+++ MATLAB/HistogramCalculation.m
@@ -70,3 +70,47 @@ xlabel('Pixel Value');
 ylabel('Count');
 
 
+r = histeq(original_image(:,:,1));
+g = histeq(original_image(:,:,2));
+b = histeq(original_image(:,:,3));
+
+
+[countsR,binsR] =  imhist(r);
+[countsG,binsG] =  imhist(g);
+[countsB,binsB] =  imhist(b);
+
+figure(3)
+subplot(2,2,1);
+stem(binsR,countsR, 'r');
+title('Red')
+xlabel('Pixel Value');
+ylabel('Count');
+subplot(2,2,2);
+stem(binsG, countsG, 'g');
+title('Green')
+xlabel('Pixel Value');
+ylabel('Count');
+subplot(2,2,3);
+stem(binsB, countsB, 'b');
+title('Blue')
+xlabel('Pixel Value');
+ylabel('Count');
+subplot(2,2,4);
+hold on
+stem(binsR,countsR, 'r');
+stem(binsG, countsG, 'g');
+stem(binsB, countsB, 'b');
+% plot(binsR,countsR, 'r', binsG, countsG, 'g', binsB, countsB, 'b');
+title('All')
+xlabel('Pixel Value');
+ylabel('Count');
+
+
+foo(:,:,1) = r;
+foo(:,:,2) = g;
+foo(:,:,3) = b;
+figure(4)
+imshow(foo)
+
+
+
